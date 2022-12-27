5fc2c46563eb259747b7a8589fd3f4bad41cedc2
Fulya <fulya.ertay@gmail.com>
2022-12-28 00:24:46 +0300

    create: data.js created

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default[
+    {"id":1,
+     "title":"Rose Day",
+     "image":'https://images.hindustantimes.com/img/2022/02/06/550x309/Rose_Day_960_1644147911234_1644147930125.jpg',
+     "date":"February 7"
+    },
+    {"id":2,
+     "title":"Propose Day",
+     "image":'https://images.hindustantimes.com/img/2022/02/06/550x309/Propose_Day_960_1644154762636_1644154817474.jpg',
+     "date":"February 8"
+    },
+    {"id":3,
+     "title":"Chocolate Day",
+     "image":'https://i.pinimg.com/originals/c4/5c/57/c45c574f013374c12f2762d99324bbed.jpg',
+     "date":"February 9"
+    },
+    {"id":4,
+    "title":" Teddy Day",
+    "image":'https://static.toiimg.com/photo/msid-73984710/73984710.jpg',
+    "date":"February 10"
+    },
+    {"id":5,
+    "title":"Promise Day",
+    "image":'https://images.thequint.com/thequint/2022-02/43777803-a26b-41ee-90fc-f4d558ed4cf6/0652_Promise_day_1.jpg?auto=format,compress&fmt=webp&format=webp&w=1200&h=900&dpr=1.0',
+    "date":"February 11"
+    },
+    {"id":6,
+    "title":" Hug Day",
+    "image":'https://images.hindustantimes.com/img/2022/02/09/1600x900/Hug_Day_960_1644405840445_1644405851362.jpg',
+    "date":"February 12"
+    },
+    {"id":7,
+    "title":" Kiss Day",
+    "image":'https://imgk.timesnownews.com/story/Happy_Kiss_Day_2022.png',
+    "date":"February 13"
+    },
+    {"id":8,
+    "title":" Valentine's Day",
+    "image":'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/feb-14-valentine%27s-day.-happy-valentine%27s-day-design-template-e02e5705e6fe21b2721264ca2a09a93b_screen.jpg?ts=1641311041',
+    "date":"February 14"
+    },
+
+
+]
