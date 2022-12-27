6b60d60d34c29d6cb9a3d297a71ee6f87e2c4272
Fulya <fulya.ertay@gmail.com>
2022-12-28 00:37:32 +0300

    feat: create map function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import data from './data';
 
 function App() {
+  const [dates,setDates]=useState(data);
   return (
     <main>
     <section className='container'>
       <h3>
-      Valentine's Week Full List
+        Valentine's Week Full List 
       </h3>
+      {dates.map((date)=>{
+        return(
+          <div className='date' key={date.id}>
+            <img src={date.image}></img>
+            <div>
+              <p>{date.title}</p>
+              <p>{date.date}</p>
+            </div>
+          </div>
+        )
+      })}
+     
       <button>
         Clear All
       </button>
